739e8df7390329389eb5ee7900dc29046ccbd816
Tim Warrington <tim.warrin@gmail.com>
2019-01-30 19:40:39 -0700

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# Hugo Webpack Starter
+
+A very minimal setup of [hugo](https://gohugo.io/) and [webpack](https://webpack.js.org/) for assets.
+
+## Prerequisites
+
+- [hugo](https://gohugo.io/)
+- [node (NPM)](https://nodejs.org/en/)
+
+## Running
+
+1. You must have hugo and node (NPM) installed
+2. Run `npm start` in one terminal window
+    - Or run `npm run start:webpack` and `npm run start:hugo` in separate terminal windows
+3. Open a browser to `localhost:1313`
+4. The generated site is served from the `public` directory (hugo's default)
+
+## Building
+
+1. You must have hugo and node (NPM) installed
+2. Run `npm run build`
+3. The generated site is served from the `public` directory (hugo's default)
+
+## Cleaning
+
+You can run `npm run clean` to remove any hugo and webpack generated files
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
+module.exports = (env, argv) => {
+  const devMode = argv.mode != 'production';
+
+  return {
+    mode: argv.mode,
+    devtool: devMode ? 'cheap-eval-source-map' : 'source-map',
+    entry: {
+      main: ['./assets/js/main.js', './assets/scss/main.scss'],
+      images: './assets/images/index.js',
+    },
+    output: {
+      filename: devMode ? 'js/[name].js' : 'js/[name].[chunkhash:8].js',
+      publicPath: '/',
+      path: path.resolve(__dirname, 'static/assets'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: path.resolve(__dirname, 'assets'),
+          use: 'babel-loader',
+        },
+        {
+          test: /\.s?css$/,
+          include: path.resolve(__dirname, 'assets'),
+          use: [
+            { loader: MiniCssExtractPlugin.loader },
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true }
+            },
+            {
+              loader: 'postcss-loader',
+              options: { sourceMap: true }
+            },
+            {
+              loader: 'resolve-url-loader',
+              options: { sourceMap: true }
+            },
+            {
+              loader: 'sass-loader',
+              options: { sourceMap: true }
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/,
+          include: path.resolve(__dirname, 'assets'),
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 15000,
+              name: devMode ? 'images/[name].[ext]' : 'images/[name].[hash:8].[ext]',
+            }
+          },
+        },
+      ],
+    },
+    plugins: [
+      new FixStyleOnlyEntriesPlugin(),
+      new MiniCssExtractPlugin({
+        filename: devMode ? 'css/[name].css' : 'css/[name].[contenthash:8].css',
+        chunkFilename: devMode ? 'css/[id].css' : 'css/[id].[contenthash:8].css',
+      }),
+      new ManifestPlugin({
+        publicPath: 'assets/',
+        fileName: path.resolve(__dirname, 'data/manifest.json'),
+      })
+    ],
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+      minimizer: [
+        new OptimizeCSSAssetsPlugin({
+          cssProcessorOptions: {
+            map: {
+              inline: false,
+              annotation: true,
+            },
+          },
+        }),
+        new UglifyWebpackPlugin({ sourceMap: true })
+      ],
+    },
+  }
+}
